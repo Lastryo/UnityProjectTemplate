ff254b559b0dab3ceb94b5469326ae8389e5ee0a
Lastryo <Lastryo@live.com>
2025-08-22 00:56:12 +0300

    Update GlobalUpdater.cs

--- Unity/Core/GlobalUpdate/GlobalUpdater.cs
+++ Unity/Core/GlobalUpdate/GlobalUpdater.cs
@@ -1,53 +1,108 @@
 using System;
-using System.Collections.Generic;
-using Core.Interfaces;
-using UnityEngine;
+using System.Buffers;
 
-namespace Core
+[Serializable]
+public class GlobalUpdater
 {
-    [Serializable]
-    public class GlobalUpdater
+    private readonly UpdaterCollection<IUpdatable> updatables = new UpdaterCollection<IUpdatable>();
+    private readonly UpdaterCollection<IFixedUpdatable> fixedUpdatables = new UpdaterCollection<IFixedUpdatable>();
+    private readonly UpdaterCollection<ILateUpdatable> lateUpdatables = new UpdaterCollection<ILateUpdatable>();
+
+    private class UpdaterCollection<T> where T : class
     {
-        private List<IUpdatable> updatables = new List<IUpdatable>();
-        private List<IFixedUpdatable> fixedUpdatables = new List<IFixedUpdatable>();
-        private List<ILateUpdatable> lateUpdatables = new List<ILateUpdatable>();
+        private const int InitialCapacity = 16; // Начальная ёмкость массива
+        private T[] array; // Арендованный массив из пула
+        private int count; // Текущее количество элементов
 
-        public void Register(object obj)
+        public UpdaterCollection()
         {
-            if (obj is IUpdatable updatable)
-                updatables.Add(updatable);
-            if (obj is IFixedUpdatable fixedUpdatable)
-                fixedUpdatables.Add(fixedUpdatable);
-            if (obj is ILateUpdatable lateUpdatable)
-                lateUpdatables.Add(lateUpdatable);
+            array = ArrayPool<T>.Shared.Rent(InitialCapacity);
+            count = 0;
         }
 
-        public void Unregister(object obj)
+        public void Add(T item)
         {
-            if (obj is IUpdatable updatable)
-                updatables.Remove(updatable);
-            if (obj is IFixedUpdatable fixedUpdatable)
-                fixedUpdatables.Remove(fixedUpdatable);
-            if (obj is ILateUpdatable lateUpdatable)
-                lateUpdatables.Remove(lateUpdatable);
+            if (count >= array.Length)
+            {
+                // Удваиваем размер массива, если он заполнен
+                var newArray = ArrayPool<T>.Shared.Rent(array.Length * 2);
+                Array.Copy(array, newArray, count);
+                ArrayPool<T>.Shared.Return(array);
+                array = newArray;
+            }
+            array[count] = item;
+            count++;
         }
 
-        public void Update()
+        public void Remove(T item)
         {
-            foreach (var updatable in updatables)
-                updatable.OnUpdate();
+            int index = array.AsSpan(0, count).IndexOf(item);
+            if (index >= 0)
+            {
+                // Swap-and-pop: заменяем удаляемый элемент последним
+                count--;
+                array[index] = array[count];
+                array[count] = null;
+            }
         }
 
-        public void FixedUpdate()
+        public void Update(Action<T> updateAction)
         {
-            foreach (var updatable in fixedUpdatables)
-                updatable.OnFixedUpdate();
+            foreach (var item in array.AsSpan(0, count))
+                updateAction(item);
         }
 
-        public void LateUpdate()
+        public void Dispose()
         {
-            foreach (var updatable in lateUpdatables)
-                updatable.OnLateUpdate();
+            if (array.Length > 0)
+                ArrayPool<T>.Shared.Return(array);
+            array = Array.Empty<T>();
+            count = 0;
         }
+    }
+
+    public void Register(object obj)
+    {
+        // Регистрируем объект во всех подходящих коллекциях
+        if (obj is IUpdatable updatable)
+            updatables.Add(updatable);
+        if (obj is IFixedUpdatable fixedUpdatable)
+            fixedUpdatables.Add(fixedUpdatable);
+        if (obj is ILateUpdatable lateUpdatable)
+            lateUpdatables.Add(lateUpdatable);
+    }
+
+    public void Unregister(object obj)
+    {
+        // Удаляем объект из всех подходящих коллекций
+        if (obj is IUpdatable updatable)
+            updatables.Remove(updatable);
+        if (obj is IFixedUpdatable fixedUpdatable)
+            fixedUpdatables.Remove(fixedUpdatable);
+        if (obj is ILateUpdatable lateUpdatable)
+            lateUpdatables.Remove(lateUpdatable);
+    }
+
+    public void Update()
+    {
+        updatables.Update(item => item.OnUpdate());
+    }
+
+    public void FixedUpdate()
+    {
+        fixedUpdatables.Update(item => item.OnFixedUpdate());
+    }
+
+    public void LateUpdate()
+    {
+        lateUpdatables.Update(item => item.OnLateUpdate());
+    }
+
+    public void Dispose()
+    {
+        // Освобождаем все арендованные массивы
+        updatables.Dispose();
+        fixedUpdatables.Dispose();
+        lateUpdatables.Dispose();
     }
 }
